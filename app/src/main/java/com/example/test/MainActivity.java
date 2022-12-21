8cd6a5032f7d4e7d47619a14af9a722c009fea9d
AkshayM1 <122124@lrstudents.org>
2022-12-21 15:46:50 -0500

    Base of the app
    Started the app

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tab_2);
+
+        //Button = (Button) findViewById(R.id.tab_2);
+
+    }
+}
