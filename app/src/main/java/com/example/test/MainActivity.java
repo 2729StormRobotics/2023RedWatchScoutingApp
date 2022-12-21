0a21f817ed1095a95a4bcde9a5acfff62af71b21
AkshayM1 <122124@lrstudents.org>
2022-12-21 16:50:27 -0500

    Made a new activity
    To test going from one page to another

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.test;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(R.layout.tab_2);
 
-        //Button = (Button) findViewById(R.id.tab_2);
+        button = (Button) findViewById(R.id.button_Id);
+
+        button.setOnClickListener(view -> {
+
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            startActivity(intent);
+
+
+
+        });
+
 
     }
 }
