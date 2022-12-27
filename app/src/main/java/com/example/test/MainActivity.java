6e6ff7ade3d1c799c6dae63e4917f36d17a67b2d
NeonGems <neongamers65@gmail.com>
2022-12-27 16:50:04 -0500

    Added username feature
    There is a new textbox, if you enter text into the textbox and click submit, a welcome message will appear at the top saying, "Welcome" and print out what you put into the textbox

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,22 +1,44 @@
 package com.example.test;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonToSoftware;
+    Button buttonToBAM;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        // Creates two references for an EditText view and a TextView view
+        EditText insertUsername = findViewById(R.id.insertUsername);
+        TextView welcomeMessage = findViewById(R.id.welcomeMessage);
+
+        // When user clicks
+        Button buttonToSubmit = findViewById(R.id.submitUsername);
+        buttonToSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The user has clicked the button
+                String userName = insertUsername.getText().toString();
+                welcomeMessage.setText("Hello " + userName);
+            }
+        });
+
         // Code for button which will lead to software page
-        buttonToSoftware = (Button) findViewById(R.id.toSoftware);
+        buttonToSoftware = findViewById(R.id.toSoftware);
 
         buttonToSoftware.setOnClickListener(view -> {
 
@@ -28,9 +50,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Code for button which will lead to BAM page
-        buttonToSoftware = (Button) findViewById(R.id.toBAM);
+        buttonToBAM = findViewById(R.id.toBAM);
 
-        buttonToSoftware.setOnClickListener(view -> {
+        buttonToBAM.setOnClickListener(view -> {
 
             Intent intent = new Intent(MainActivity.this, MainActivity3.class);
             startActivity(intent);
--- app/src/main/java/com/example/test/MainActivity3.java
+++ app/src/main/java/com/example/test/MainActivity3.java
@@ -3,6 +3,7 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+
     }
 }
