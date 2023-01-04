4de4ca8eb2326d61ea6d95f3dfa2cb5bb40cf3dd
AkshayM1 <122124@lrstudents.org>
2023-01-04 16:57:37 -0500

    Attempted to create login system
    There are multiple errors in the MainActivity class, if you want to run make sure you comment the errors out.

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,26 +1,41 @@
 package com.example.test;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonToSoftware;
     Button buttonToBAM;
+    private FirebaseAuth auth;
+    private ProgressDialog progressDialog;
 
     @SuppressLint("SetTextI18n")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState, Object name, Object password) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        ProgressDialog processDialog = new ProgressDialog(this);
+
+        // Create variables
+        String userName = "";
+        String passWord = "";
 
         // Creates references for EditText views and TextView views
         EditText insertUsername = findViewById(R.id.insertUsername);
@@ -33,8 +48,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // The user has clicked the button
-                String userName = insertUsername.getText().toString();
+                userName = insertUsername.getText().toString();
+                passWord = insertPassword.getText().toString();
 
+                validate(userName, passWord);
                 welcomeMessage.setText("Hello " + userName + "!");
             }
         });
@@ -62,6 +79,27 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+
+
+        public void validate(String s, String d) {
+            processDialog.setMessage("................Please wait lmao.......");
+            processDialog.show();
+
+            auth.signInWithEmailAndPassword(userName, passWord).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        processDialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this, MainActivity.class));
+                    } else {
+                        Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                        processDialog.dismiss();
+
+                    }
+                }
+            })
+        }
 
     }
 }
